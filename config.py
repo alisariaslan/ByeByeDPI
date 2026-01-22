4c4027826310ca6ff38e0c90ddf6e81cbf303b2b
alisariaslann <125967344+alisariaslann@users.noreply.github.com>
2026-01-22 18:06:20 +0300

    config del

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-LM_STUDIO_API_URL = "https://lmstudio.alisariaslan.com.tr/v1/chat/completions"
-MODEL = "qwen2.5-coder-1.5b-instruct"
-LM_STUDIO_BEARER_TOKEN = "sk-lm-gizli-anahtar-42"
-PFX_PASSWORD = "g7#Vn9rQ!tY4bZx2@HdL6mK"
